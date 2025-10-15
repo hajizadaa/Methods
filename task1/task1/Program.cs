29c7fe8a50e9ef7a8f73fc37df1ee9250c7a3939
hajizadaa <asifeh-bpa202@code.edu.az>
2025-10-16 01:42:45 +0400

    task 1 added

---
+++ task1/task1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace task1;
+//Verilən ədədin cüt olub olmadığını yoxlayan method yazın. Return type: bool.
+
+class Program
+{
+    static bool IsEven(int num)
+    {
+        if (num % 2 == 0)
+            return true;
+        return false;
+    }
+    
+    static void Main(string[] args)
+    {
+        Console.Write("Ededi Daxil Edin: ");
+        int n = int.Parse(Console.ReadLine());
+        bool answer = IsEven(n);
+        Console.WriteLine(answer);
+    }
+}
