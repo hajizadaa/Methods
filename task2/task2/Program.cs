b664313822de52ebbe278f6c14227ab26a1099fd
hajizadaa <asifeh-bpa202@code.edu.az>
2025-10-16 01:43:33 +0400

    task 2 added

---
+++ task2/task2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace task2;
+//Fahrenheit ilə verilmiş dəyəri Celsius-a çevirən method yazın. Kəsr ədəd daxil etmək olsun.
+class Program
+{
+    static float ToCelsius(float f)
+    {
+        return (f - 32f) / 1.8f;
+    }
+    
+    static void Main(string[] args)
+    {
+        Console.Write("Fahrenheiti Derecesini Daxil Edin: ");
+        float f = float.Parse(Console.ReadLine());
+        float c = ToCelsius(f);
+        Console.WriteLine("Celsius: " + c);
+    }
+}
