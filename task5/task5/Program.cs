6f23867726f919e5ea50d149a496ddf12cfff1f4
hajizadaa <asifeh-bpa202@code.edu.az>
2025-10-16 01:44:40 +0400

    task 5 added

---
+++ task5/task5/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace task5;
+
+class Program
+{
+    static bool IsPrimeNumber(int n)
+    {
+        int divisor = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                divisor++;
+            }
+        }
+
+        if (divisor == 2)
+            return true;
+        return false;
+
+    }
+    static void Main(string[] args)
+    {
+        Console.Write("Ededi Daxil Edin: ");
+        int n = int.Parse(Console.ReadLine());
+        bool answer = IsPrimeNumber(n);
+        Console.WriteLine(answer);
+    }
+}
