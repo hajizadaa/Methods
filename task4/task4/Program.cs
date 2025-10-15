0269fcb05a253874382b2fc2683d23d9683909a2
hajizadaa <asifeh-bpa202@code.edu.az>
2025-10-16 01:44:19 +0400

    task 4 added

---
+++ task4/task4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace task4;
+//Verilmiş ədədin faktorialını hesablayan method yazın.
+class Program
+{
+    static int Fakt(int n)
+    {
+        if (n > 1)
+            return n * Fakt(n - 1);
+        return 1;
+    }
+    
+    static void Main(string[] args)
+    {
+        Console.Write("n = ");
+        int n = int.Parse(Console.ReadLine());
+        int fakt = Fakt(n);
+        Console.WriteLine("n! = " + fakt);
+    }
+}
